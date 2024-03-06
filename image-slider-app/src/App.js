830701acd53ec84ee753e3eea7fcacadfbf23de3
hibana_remote_college_vps <hibana@dr.com>
2024-03-07 00:57:04 +0300

    found prettier extension

--- image-slider-app/src/App.js
+++ image-slider-app/src/App.js
@@ -1,10 +1,17 @@
-import React, { useEffect, useState, useRef } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState, useRef } from "react";
+import axios from "axios";
 //import './App.css';
 
 function App() {
-  const [characters] = useState(['Ichika', 'Nino', 'Miku', 'Yotsuba', 'Itsuki', 'Together']); // Added characters array
-  const [selectedCharacter, setSelectedCharacter] = useState('Ichika'); // Default selected character
+  const [characters] = useState([
+    "Ichika",
+    "Nino",
+    "Miku",
+    "Yotsuba",
+    "Itsuki",
+    "Together",
+  ]); // Added characters array
+  const [selectedCharacter, setSelectedCharacter] = useState("Ichika"); // Default selected character
   const [images, setImages] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
@@ -12,13 +19,16 @@ function App() {
   // Fetch the list of images from the server for the selected character
   useEffect(() => {
     setIsLoading(true); // Show loader while fetching
-    axios.get(`http://192.168.88.119:5000/images/${selectedCharacter}/list`)
-      .then(response => {
+    axios
+      .get(`http://192.168.88.119:5000/images/${selectedCharacter}/list`)
+      .then((response) => {
         setImages(response.data);
         setCurrentIndex(Math.floor(Math.random() * response.data.length));
         setIsLoading(false); // Hide loader after fetching
       })
-      .catch(error => console.error("There was an error fetching the images: ", error));
+      .catch((error) =>
+        console.error("There was an error fetching the images: ", error)
+      );
   }, [selectedCharacter]); // Dependency array now includes selectedCharacter
 
   const handleCharacterChange = (character) => {
@@ -43,10 +53,10 @@ function App() {
   const downloadCurrentImage = () => {
     const imageSrc = `http://192.168.88.119:5000${images[currentIndex]}`;
     fetch(imageSrc)
-      .then(response => response.blob())
-      .then(blob => {
+      .then((response) => response.blob())
+      .then((blob) => {
         const blobUrl = window.URL.createObjectURL(blob);
-        const link = document.createElement('a');
+        const link = document.createElement("a");
         link.href = blobUrl;
         link.download = `${currentIndex + 1}.jpg`; // Customize the filename here
         document.body.appendChild(link);
@@ -56,26 +66,26 @@ function App() {
           window.URL.revokeObjectURL(blobUrl);
         }, 100);
       })
-      .catch(err => console.error("Error in downloading the file", err));
+      .catch((err) => console.error("Error in downloading the file", err));
   };
 
   useEffect(() => {
     function handleKeyPress(e) {
-      if (e.key === 'ArrowRight') {
+      if (e.key === "ArrowRight") {
         nextImage();
-      } else if (e.key === 'ArrowLeft') {
+      } else if (e.key === "ArrowLeft") {
         prevImage();
-      } else if (e.key.toLowerCase() === 'r') {
+      } else if (e.key.toLowerCase() === "r") {
         randomImage();
-      } else if (e.key.toLowerCase() === 'd') {
+      } else if (e.key.toLowerCase() === "d") {
         downloadCurrentImage();
       }
     }
 
-    window.addEventListener('keydown', handleKeyPress);
-    return () => window.removeEventListener('keydown', handleKeyPress);
+    window.addEventListener("keydown", handleKeyPress);
+    return () => window.removeEventListener("keydown", handleKeyPress);
   }, [nextImage, prevImage, randomImage]);
-  
+
   const onLoad = () => {
     setIsLoading(false); // Set loading to false when the image is loaded
   };
@@ -85,38 +95,60 @@ function App() {
       <header className="flex flex-col items-center justify-center w-full px-4 text-center">
         <h2 className="text-2xl mb-2">Quintessential Quintuplets Gallery</h2>
         <div className="flex flex-wrap justify-center items-center gap-2 mb-2">
-        {characters.map(character => (
-        <button 
-          key={character} 
-          onClick={() => handleCharacterChange(character)} 
-          className={`${
-            selectedCharacter === character ? "bg-green-500" : "bg-blue-500"
-          } hover:bg-blue-700 text-white font-bold py-1 px-4 rounded text-sm`}
-        >
-          {character}
-        </button>
-         ))}
+          {characters.map((character) => (
+            <button
+              key={character}
+              onClick={() => handleCharacterChange(character)}
+              className={`${
+                selectedCharacter === character ? "bg-green-500" : "bg-blue-500"
+              } hover:bg-blue-700 text-white font-bold py-1 px-4 rounded text-sm`}
+            >
+              {character}
+            </button>
+          ))}
         </div>
         <span className="image-counter text-sm mb-2 font-style: italic">
           Image {currentIndex + 1} of {images.length}
         </span>
         {images.length > 0 && (
           <div className="image-wrapper flex justify-center items-center mb-2 w-full">
-            <img 
-              src={`http://192.168.88.119:5000${images[currentIndex]}`} 
+            <img
+              src={`http://192.168.88.119:5000${images[currentIndex]}`}
               alt="Girls"
               onLoad={onLoad}
-              className={`max-w-full max-h-[60vh] md:max-h-[74vh] object-contain transition-opacity duration-500 ease-in-out ${isLoading ? 'opacity-0' : 'opacity-100'}`}
+              className={`max-w-full max-h-[60vh] md:max-h-[74vh] object-contain transition-opacity duration-500 ease-in-out ${
+                isLoading ? "opacity-0" : "opacity-100"
+              }`}
               key={currentIndex}
             />
           </div>
         )}
         <div className="carousel-controls flex justify-center items-center gap-4 mb-4 mt-2">
-          <button onClick={prevImage} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded text-sm">⤎ Prev</button>
-          <button onClick={randomImage} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded text-sm">Random (R)</button>
-          <button onClick={nextImage} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded text-sm">Next ⤏</button>
+          <button
+            onClick={prevImage}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded text-sm"
+          >
+            ⤎ Prev
+          </button>
+          <button
+            onClick={randomImage}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded text-sm"
+          >
+            Random (R)
+          </button>
+          <button
+            onClick={nextImage}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded text-sm"
+          >
+            Next ⤏
+          </button>
         </div>
-        <button onClick={downloadCurrentImage} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded text-sm">Download Current Image (D)</button>
+        <button
+          onClick={downloadCurrentImage}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded text-sm"
+        >
+          Download Current Image (D)
+        </button>
       </header>
     </div>
   );
--- server.js
+++ server.js
@@ -1,26 +1,29 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 5000;
-const cors = require('cors');
+const cors = require("cors");
 
 // Use cors before all route definitions
 app.use(cors());
 
 // Serve images statically from each character's folder
-const characters = ['Ichika', 'Nino', 'Miku', 'Yotsuba', 'Itsuki', 'Together'];
+const characters = ["Ichika", "Nino", "Miku", "Yotsuba", "Itsuki", "Together"];
 characters.forEach((character) => {
-  app.use(`/images/${character}`, express.static(path.join(__dirname, `/images/${character}`)));
+  app.use(
+    `/images/${character}`,
+    express.static(path.join(__dirname, `/images/${character}`))
+  );
 });
 
 // List images endpoint for each character
-app.get('/images/:character/list', (req, res) => {
+app.get("/images/:character/list", (req, res) => {
   const character = req.params.character;
 
   // Ensure that the character is one of the valid options to prevent unauthorized access
   if (!characters.includes(character)) {
-    res.status(404).send('Character not found');
+    res.status(404).send("Character not found");
     return;
   }
 
@@ -29,11 +32,11 @@ app.get('/images/:character/list', (req, res) => {
   fs.readdir(imagesDirectory, (err, files) => {
     if (err) {
       console.error(`Could not list the directory for ${character}.`, err);
-      res.status(500).send('Internal server error');
+      res.status(500).send("Internal server error");
       return;
     }
 
-    const fileUrls = files.map(file => `/images/${character}/${file}`);
+    const fileUrls = files.map((file) => `/images/${character}/${file}`);
     res.json(fileUrls);
   });
 });
