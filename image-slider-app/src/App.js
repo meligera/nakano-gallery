554d1ab6bdab7d19b07dd01953e516c059e75703
German <meligera@ya.ru>
2024-04-12 19:27:40 +0000

    add ability to download girls

--- image-slider-app/src/App.js
+++ image-slider-app/src/App.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-//import './App.css';
+import JSZip from 'jszip';
 
 function App() {
   const [characters] = useState([
@@ -10,55 +10,51 @@ function App() {
     "Yotsuba",
     "Itsuki",
     "Together",
-  ]); // Added characters array
-  const [selectedCharacter, setSelectedCharacter] = useState("Ichika"); // Default selected character
+  ]);
+  const [selectedCharacter, setSelectedCharacter] = useState("Ichika");
   const [images, setImages] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
 
-  // Fetch the list of images from the server for the selected character
   useEffect(() => {
-    setIsLoading(true); // Show loader while fetching
+    setIsLoading(true);
     axios
-      .get(`http://192.168.88.119:5000/images/${selectedCharacter}/list`)
+      .get(`http://192.168.77.240:5000/images/${selectedCharacter}/list`)
       .then((response) => {
         setImages(response.data);
         setCurrentIndex(Math.floor(Math.random() * response.data.length));
-        setIsLoading(false); // Hide loader after fetching
+        setIsLoading(false);
       })
       .catch((error) =>
         console.error("There was an error fetching the images: ", error)
       );
-  }, [selectedCharacter]); // Dependency array now includes selectedCharacter
+  }, [selectedCharacter]);
 
   const handleCharacterChange = (character) => {
-    setSelectedCharacter(character); // Update the selected character which triggers the useEffect to fetch images
+    setSelectedCharacter(character);
   };
 
-  // Function to navigate to the next image
   const nextImage = () => {
     setCurrentIndex((currentIndex + 1) % images.length);
   };
 
-  // Function to navigate to the previous image
   const prevImage = () => {
     setCurrentIndex((currentIndex - 1 + images.length) % images.length);
   };
 
-  // Function to show a random image
   const randomImage = () => {
     setCurrentIndex(Math.floor(Math.random() * images.length));
   };
 
   const downloadCurrentImage = () => {
-    const imageSrc = `http://192.168.88.119:5000${images[currentIndex]}`;
+    const imageSrc = `http://192.168.77.240:5000${images[currentIndex]}`;
     fetch(imageSrc)
       .then((response) => response.blob())
       .then((blob) => {
         const blobUrl = window.URL.createObjectURL(blob);
         const link = document.createElement("a");
         link.href = blobUrl;
-        link.download = `${currentIndex + 1}.jpg`; // Customize the filename here
+        link.download = `${currentIndex + 1}.jpg`;
         document.body.appendChild(link);
         link.click();
         setTimeout(() => {
@@ -67,6 +63,63 @@ function App() {
         }, 100);
       })
       .catch((err) => console.error("Error in downloading the file", err));
+  };
+
+  const downloadCurrentCharacterImages = () => {
+    const zip = new JSZip();
+    const promises = images.map((image, index) => {
+      const imageUrl = `http://192.168.77.240:5000${image}`;
+      return fetch(imageUrl)
+        .then((response) => response.blob())
+        .then((blob) => {
+          zip.file(`image_${index + 1}.jpg`, blob);
+        });
+    });
+
+    Promise.all(promises).then(() => {
+      zip.generateAsync({ type: "blob" }).then((content) => {
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(content);
+        link.download = `${selectedCharacter}_images.zip`;
+        document.body.appendChild(link);
+        link.click();
+        setTimeout(() => {
+          document.body.removeChild(link);
+        }, 100);
+      });
+    });
+  };
+
+  const downloadAllImages = () => {
+    const zip = new JSZip();
+    const promises = characters.map((character) =>
+      axios
+        .get(`http://192.168.77.240:5000/images/${character}/list`)
+        .then((response) => {
+          const characterImages = response.data.map(
+            (image, index) =>
+              fetch(`http://192.168.77.240:5000${image}`)
+                .then((response) => response.blob())
+                .then((blob) => {
+                  zip.file(`${character}_image_${index + 1}.jpg`, blob);
+                })
+          );
+          return Promise.all(characterImages);
+        })
+    );
+
+    Promise.all(promises).then(() => {
+      zip.generateAsync({ type: "blob" }).then((content) => {
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(content);
+        link.download = `all_images.zip`;
+        document.body.appendChild(link);
+        link.click();
+        setTimeout(() => {
+          document.body.removeChild(link);
+        }, 100);
+      });
+    });
   };
 
   useEffect(() => {
@@ -87,7 +140,7 @@ function App() {
   }, [nextImage, prevImage, randomImage]);
 
   const onLoad = () => {
-    setIsLoading(false); // Set loading to false when the image is loaded
+    setIsLoading(false);
   };
 
   return (
@@ -113,10 +166,10 @@ function App() {
         {images.length > 0 && (
           <div className="image-wrapper flex justify-center items-center mb-2 w-full">
             <img
-              src={`http://192.168.88.119:5000${images[currentIndex]}`}
+              src={`http://192.168.77.240:5000${images[currentIndex]}`}
               alt="Girls"
               onLoad={onLoad}
-              className={`max-w-full max-h-[60vh] md:max-h-[74vh] object-contain transition-opacity duration-500 ease-in-out ${
+              className={`max-w-full max-h-[60vh] md:max-h-[74vh] object-contain transition-all duration-500 ease-in-out ${
                 isLoading ? "opacity-0" : "opacity-100"
               }`}
               key={currentIndex}
@@ -143,12 +196,26 @@ function App() {
             Next ⤏
           </button>
         </div>
-        <button
-          onClick={downloadCurrentImage}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded text-sm"
-        >
-          Download Current Image (D)
-        </button>
+        <div className="download-buttons absolute top-4 right-4">
+          <button
+            onClick={downloadCurrentImage}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded text-sm"
+          >
+            Download Current Image (D)
+          </button>
+          <button
+            onClick={downloadCurrentCharacterImages}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded text-sm ml-2"
+          >
+            Download Current Character Images
+          </button>
+          <button
+            onClick={downloadAllImages}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded text-sm ml-2"
+          >
+            Download All Images
+          </button>
+        </div>
       </header>
     </div>
   );
