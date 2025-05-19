8d2cda596548b4e107aeb77091b5e610ad165d92
meligera <meligera@ya.ru>
2025-05-20 02:28:22 +0300

    Restyle gallery with ChatGPT-inspired theme

--- image-slider-app/tailwind.config.js
+++ image-slider-app/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui"],
+      },
+    },
   },
   plugins: [],
 }
